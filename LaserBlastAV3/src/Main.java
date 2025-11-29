cc7b42db04594d6731ab4c73af95cc3377d784fc
ccarvalho-jp <carvalhojp2410@gmail.com>
2025-11-29 10:41:06 -0300

    Add test comment for menu in Main class

--- LaserBlastAV3/src/Main.java
+++ LaserBlastAV3/src/Main.java
@@ -2,21 +2,18 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        // Criar o ranking e carregá-lo do ficheiro
+      //TESTE DO MENU NA MAIN
         RankingManager ranking = new RankingManager();
         ranking.carregar();
 
-        // Criar a janela
         JFrame janela = new JFrame("Menu do Jogo");
         janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         janela.setSize(500, 600);
         janela.setLocationRelativeTo(null); // centraliza na tela
 
-        // Criar a tela de menu e adicioná-la à janela
         MenuScreen menu = new MenuScreen(ranking);
         janela.add(menu);
 
-        // Mostrar a janela
         janela.setVisible(true);
     }
 }
